ccb134bc85a8b4e738b4f6625048c25431e6bd71
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-01 16:22:36 -0800

    [add] tailwind library and update homescreen with logo

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>RideShare App</Text>
-        <StatusBar style="auto" />
-      </View>
+      <StatusBar style="auto" />
+      <HomeScreen />
     </Provider>
   );
 }
--- store.js
+++ store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import navReducer from "./slices/navSlice";
+import navReducer from "./slices/navSlice.js";
 
 
 export const store = configureStore({
