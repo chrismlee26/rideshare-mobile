bd3c7a7bbd348d2469993754985268705eee8358
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-02 01:51:31 -0800

    [add] ride options component to map screen and add navigation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
 
   return (
     <View>
-      <Text>MapScreen</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
