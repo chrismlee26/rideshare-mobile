c037fe4c2638f4266e6f5290b439671a28171f8b
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-01 20:47:05 -0800

    [add] setOrigin with google places api, bugfix reducers typo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,9 +4,12 @@ import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env';
-
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -16,6 +19,27 @@ const HomeScreen = () => {
         />
         <GooglePlacesAutocomplete
           placeholder='Where from?'
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description,
+            }));
+            dispatch(setDestination(null))
+          }}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
           nearbyPlacessAPI="GooglePlacesSearch"
           debounce={400}
 
