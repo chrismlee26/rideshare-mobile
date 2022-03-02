4a3aa219487512b03c7d355d013b480dda16a09f
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-01 19:20:52 -0800

    [add] react-native-elements and icon to NavOptions component

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
@@ -8,7 +10,9 @@ export default function App() {
   return (
     <Provider store={store}>
       <StatusBar style="auto" />
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
-import { FlatList, TouchableOpacity, View, Text, Image } from 'react-native'
-import React from 'react'
+import { FlatList, TouchableOpacity, View, Text, Image } from 'react-native';
+import React from 'react';
 import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
 
 const data = [
   {
@@ -37,6 +38,12 @@ const NavOptions = () => {
             <Text style={tw`mt-2 text-lg font-semibold`}>
               {item.title}
             </Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name='arrowright'
+              color='white'
+              type='antdesign'
+            />
           </View>
         </TouchableOpacity >
       )}
